6bd087fe7966748647826dc1dccb0571fc9fd364
Dominic Too <toodom02@gmail.com>
2020-07-13 15:45:13 +0100

    Fixed parallax on IE

---
+++ flasksite/static/main.js
@@ -0,0 +1,17 @@
+// fixes parallax on IE
+if(navigator.userAgent.match(/Trident\/7\./)) {
+    $('body').on("mousewheel", function () {
+        event.preventDefault();
+        var wd = event.wheelDelta;
+        var csp = window.pageYOffset;
+        window.scrollTo(0, csp - wd);
+    });
+}
+
+// collapse responsive navbar
+ $(function(){
+     var navMain = $(".navbar-collapse");
+     navMain.on("click", "a:not([data-toggle])", null, function () {
+         navMain.collapse('hide');
+     });
+ });
